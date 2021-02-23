861bc2db8edc9a9b29f6ca30235d57fe1e2b31c6
Hermes Caretta <hermescaretta@gmail.com>
2021-02-23 12:46:44 -0300

    inicio segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express, { request, response } from 'express'
+import express from 'express';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
 
-app.listen(3001, () => console.log('HEH'))
+app.listen(3001);
 
 app.get('/', (request, response) => {
-  return response.send('Hello World')
-})
+  return response.send('Hello World');
+});
 
-app.post('/', (request,response) => {
-  return response.json({message: "Os dados foram recebidos com sucesso"})
-})
+app.post('/', (request, response) => {
+  return response.json({ message: 'Os dados foram recebidos com sucesso' });
+});
