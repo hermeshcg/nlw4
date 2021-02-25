24bfdd45393875791921aae1c2abd7d67d774439
Hermes Caretta <hermescaretta@gmail.com>
2021-02-25 10:40:04 -0300

    rota, controller, migration, model e repository  das surveys

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,29 +1,30 @@
-import {Request, Response} from 'express'
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../repositories/UsersRepository';
 
 class UserController {
-  async create(request: Request, response: Response){
-    const {name, email} = request.body;
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
 
-    const usersRepository = getRepository(User);
-    
-    const userAlreadyExists = await usersRepository.findOne({email})
+    const usersRepository = getCustomRepository(UsersRepository);
 
-    if(userAlreadyExists){
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
       return response.status(400).json({
-        error: "User already exists"
-      })
+        error: 'User already exists',
+      });
     }
 
     const user = usersRepository.create({
-      name, email 
-    }) 
+      name,
+      email,
+    });
 
-    await usersRepository.save(user)
+    await usersRepository.save(user);
 
-    return response.json(user)
+    return response.json(user);
   }
 }
 
-export {UserController};
+export { UserController };
